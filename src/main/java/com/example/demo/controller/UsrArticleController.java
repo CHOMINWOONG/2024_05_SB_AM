3fbfb94d559c1f0a3d1585a67f6e0d612a7ed3f7
CHOMINWOONG <alsdnd0701@naver.com>
2024-06-20 16:39:07 +0900

    ArticleDao SQL, doJoin, Util.isEmpty, getMemberByLoginId

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -1,83 +1,64 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.example.demo.service.ArticleService;
 import com.example.demo.vo.Article;
 
 @Controller
 public class UsrArticleController {
 	
-	private int lastArticleId;
-	private List<Article> articles;
+	private ArticleService articleService;
 	
-	public UsrArticleController() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
-		
-		makeTestData();
+	public UsrArticleController(ArticleService articleService) {
+		this.articleService = articleService;
 	}
 	
-	private void makeTestData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "제목" + i;
-			String body = "내용" + i;
-			
-			writeArticle(title, body);
-		}
-	}
-
-	private Article writeArticle(String title, String body) {
-		Article article = new Article(++lastArticleId, title, body);
-		this.articles.add(article);
-		return article;
-	}
-	
-	private Article getArticleById(int id) {
-		for (Article article : this.articles) {			
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-
-	private void modifyArticle(Article foundArticle, String title, String body) {
-		foundArticle.setTitle(title);
-		foundArticle.setBody(body);
-	}
-	
-	private void deleteArticle(Article foundArticle) {
-		this.articles.remove(foundArticle);
-	}
-
 	@GetMapping("/usr/article/doWrite")
 	@ResponseBody
 	public Article doWrite(String title, String body) {
-		return writeArticle(title, body);
+		
+		articleService.writeArticle(title, body);
+		
+		int id = articleService.getLastInsertId();
+		
+		return articleService.getArticleById(id);
 	}
 	
 	@GetMapping("/usr/article/showList")
 	@ResponseBody
 	public List<Article> showList() {
-		return this.articles;
+		return articleService.getArticles();
+	}
+	
+	@GetMapping("/usr/article/showDetail")
+	@ResponseBody
+	public Object showDetail(int id) {
+		
+		Article foundArticle = articleService.getArticleById(id);
+		
+		if (foundArticle == null) {
+			return id + "번 게시물은 존재하지 않습니다";
+		}
+		
+		return foundArticle;
 	}
 	
 	@GetMapping("/usr/article/doModify")
 	@ResponseBody
 	public String doModify(int id, String title, String body) {
 		
-		Article foundArticle = getArticleById(id);
+		Article foundArticle = articleService.getArticleById(id);
 		
 		if (foundArticle == null) {
 			return id + "번 게시물은 존재하지 않습니다";
 		}
 		
-		modifyArticle(foundArticle, title, body);
+		articleService.modifyArticle(id, title, body);
 		
 		return id + "번 게시물을 수정했습니다";
 	}
@@ -86,27 +67,14 @@ public class UsrArticleController {
 	@ResponseBody
 	public String doDelete(int id) {
 		
-		Article foundArticle = getArticleById(id);
+		Article foundArticle = articleService.getArticleById(id);
 		
 		if (foundArticle == null) {
 			return id + "번 게시물은 존재하지 않습니다";
 		}
 		
-		deleteArticle(foundArticle);
+		articleService.deleteArticle(id);
 		
 		return id + "번 게시물을 삭제했습니다";
-	}
-	
-	@GetMapping("/usr/article/showDetail")
-	@ResponseBody
-	public Object showDetail(int id) {
-
-		Article foundArticle = getArticleById(id);
-
-		if (foundArticle == null) {
-			return id + "번 게시물은 존재하지 않습니다";
-		}
-
-		return foundArticle;
 	}
 }
---
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.ArticleDao;
+import com.example.demo.vo.Article;
+
+@Service
+public class ArticleService {
+	
+	private ArticleDao articleDao;
+	
+	public ArticleService(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+	
+	public void writeArticle(String title, String body) {
+		this.articleDao.writeArticle(title, body);
+	}
+	
+	public List<Article> getArticles() {
+		return this.articleDao.getArticles();
+	}
+	
+	public Article getArticleById(int id) {
+		return this.articleDao.getArticleById(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		this.articleDao.modifyArticle(id, title, body);
+	}
+	
+	public void deleteArticle(int id) {
+		this.articleDao.deleteArticle(id);
+	}
+
+	public int getLastInsertId() {
+		return this.articleDao.getLastInsertId();
+	}
+}
