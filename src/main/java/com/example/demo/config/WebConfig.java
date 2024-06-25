2bb0ddd388a86236fbf84632bd54d268eeb43c05
CHOMINWOONG <alsdnd0701@naver.com>
2024-06-25 16:51:15 +0900

    NeedLogin, NeedLogout, jsPrintReplace

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -5,18 +5,26 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.demo.interceptor.BeforActionInterceptor;
+import com.example.demo.interceptor.NeedLoginInterceptor;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	private BeforActionInterceptor beforActionInterceptor;
+	private NeedLoginInterceptor needLoginInterceptor;
 
-	public WebConfig(BeforActionInterceptor beforActionInterceptor) {
+	public WebConfig(BeforActionInterceptor beforActionInterceptor, NeedLoginInterceptor needLoginInterceptor) {
 		this.beforActionInterceptor = beforActionInterceptor;
+		this.needLoginInterceptor = needLoginInterceptor;
 	}
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(beforActionInterceptor).addPathPatterns("/**").excludePathPatterns("/resource/**");
+		
+		registry.addInterceptor(beforActionInterceptor).addPathPatterns("/usr/article/write")
+		.excludePathPatterns("/usr/article/doWrite").excludePathPatterns("/usr/article/modify")
+		.excludePathPatterns("/usr/article/doModify").excludePathPatterns("/usr/article/doDelete")
+		.excludePathPatterns("/usr/article/doLogout");
 	}
 }
